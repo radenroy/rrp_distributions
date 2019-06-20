869955f40271e0c1d5c7ec5b11da2b6057da7ae9
Raden Roy Pradana <45507440+radenroy@users.noreply.github.com>
2019-06-19 18:38:01 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rrp_distributions',
+      version='0.2',
+      description='Gaussian and Binomial rrp_distributions',
+      packages=['rrp_distributions'],
+      author= 'Raden Roy Pradana',
+      zip_safe=False)
